c2fc8b1e026dd1b1051347f18cf91e2d15927126
Senendra <senendra.007@gmail.com>
2020-11-02 06:05:53 +0530

    [Senendra] Add: Implementing Hash Table

---
+++ HashTables/MyMapNode.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+namespace HashTables
+{
+   public class MyMapNode<K,V>
+    {
+        public readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] item;
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.item = new LinkedList<KeyValue<K, V>>[size];
+        }
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedList.AddLast(item);
+        }
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+        public int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public LinkedList<KeyValue<K,V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = item[position];
+            if(linkedList == null )
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                item[position] = linkedList;
+            }
+            return linkedList;
+        }
+    }
+    public struct KeyValue<k, v> 
+    {
+        public k Key { get; set; }
+        public v Value { get; set; }
+    }
+}
